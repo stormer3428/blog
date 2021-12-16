724071ff0982a3131ee7d301aa227116319aee31
stormer3428 <azertyuiopcharles@gmail.com>
2021-12-16 10:23:41 +0100

    .

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,27 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+
+    private $contactRepo;
+
+    public function __construct(ContactRepository $repo)
+    {
+        $this->contactRepo = $repo;
+    }
+
+
     /**
      * @Route("/contact", name="contact")
      */
     public function index(): Response
     {
-        return $this->render('contact/index.html.twig', []);
+        return $this->render('contact/index.html.twig', [
+            "contacts" => $this->contactRepo->findAll(),
+        ]);
     }
 
     /**
-     * @Route("/contact/{city}", name="contactWithCity")
+     * @Route("/contact/{id}", name="contactId")
      */
-    public function Cities(string $city): Response
+    public function Id(string $id): Response
     {
         return $this->render('contact/index.html.twig', [
-            'city' => $city,
+            'id' => $this->contactRepo->find($id),
         ]);
     }
 }
